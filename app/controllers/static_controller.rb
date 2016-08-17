defff00b24ebe705b098815ba18e77dd016e9ea9
JWizerd <jwodke@outlook.com>
2016-08-17 10:46:16 -0600

    add initial routes

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,16 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def fort_collins_dui_lawyer
+  end
+
+  def fort_collins_criminal_justice_lawyer
+  end
+end
