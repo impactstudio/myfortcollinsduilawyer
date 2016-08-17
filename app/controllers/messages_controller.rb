9bf24e8fcb0f28e491279ab3521aaf528a7ca782
theRingleman <sam.ringleman@gmail.com>
2016-08-17 13:16:30 -0600

    add initial contact form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,15 @@
 class MessagesController < ApplicationController
   def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:name, :email, :subject, :message)
   end
 end
