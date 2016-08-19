d964e5d538a6f4774f0fce1bacfcf3b4027c960f
JWizerd <jwodke@outlook.com>
2016-08-18 21:48:25 -0600

    OO of CSS - GENERAL / REUSABLES SECTION added, practice area home links created

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message
   include ActiveModel::Model
-  attr_accessor :name, :email, :subject, :content
-  validates :name, :email, :subject, :content, presence: true
+  attr_accessor :name, :email, :subject, :message, :phone_number
+  validates :name, :email, :subject, :message, :phone_number, presence: true
 end
