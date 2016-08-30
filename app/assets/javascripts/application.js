37ecc642ab770438ea76253a7deaaf289a4af4c1
JWizerd <jwodke@outlook.com>
2016-08-30 08:29:08 -0600

    simple jquery animations for containers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+  $('.hero').animate({ height: '450px', opacity: '1'  }, 1000, 'linear');
+});
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    $(".taller-container").each( function(i){
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_object){
+        $(this).animate({'opacity': '1'}, 700);
+      }
+    });
+  });
+});
