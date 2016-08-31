caceb638a88a89e996b24ae5d32642c50bed6b01
theRingleman <sam.ringleman@gmail.com>
2016-08-31 16:19:48 -0600

    seperate homepage into partials

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static#about'
   get '/contact', to: 'static#contact'
   get '/fort-collins-dui-lawyer', to: 'static#fort_collins_dui_lawyer'
-  get '/fort-collins-criminal-justice-lawyer', to: 'static#fort_collins_criminal_justice_lawyer'
+  get '/fort-collins-criminal-justice-lawyer',
+      to: 'static#fort_collins_criminal_justice_lawyer'
   get '/thank_you', to: 'static#thank_you'
 end
