a4f5e4ab050873ab81caadbc095c0a07d1ba7ed8
theRingleman <sam.ringleman@gmail.com>
2016-09-24 11:12:13 -0600

    add case results to the seeds and display dynamically

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+CaseResult.create(result: "Our client was charged with four felony counts: (1) Theft, (2) Identity Theft, (3) Forgery, and (4) Commercial Bribery, and was facing more than twenty years in prison if convicted on all counts. We retained the services of a forensic document examiner and a private investigator. After meeting with us to review the opinion of the expert and the investigative reports, the District Attorney agreed to dismiss all of the charges.")
+CaseResult.create(result: "Three Counts of Felony Identity Theft, and Three Counts of Felony Theft. Our client was facing more than 32 years in prison. After a thorough investigation and several meetings with the District Attorney, we were able to negotiate a plea for our client that resulted in no jail time, and one year of probation.")
+CaseResult.create(result: "Stalking/Domestic Violence. After providing a through investigation and analysis of the law, we were able to convince the District Attorney to dismiss the charges.")
+CaseResult.create(result: "Assault in the Third Degree - Domestic Violence. If convicted, our client was facing possible deportation. We were able to obtain a deferred sentence for our client, resulting in no conviction or deportation proceeding being initiated.
+")
+CaseResult.create(result: "Assault in the Third Degree - Domestic Violence. Client was acquitted following a jury trial.")
+CaseResult.create(result: "Resisting Arrest and Obstructing a Peace Officer. Case was dismissed by the District Attorney after a careful review of the video tape that was in evidence.")
+CaseResult.create(result: "DUI - after negotiating with the District Attorney, the client was granted a deferred sentence and the charges were ultimately dismissed.")
+CaseResult.create(result: "the District Attorney agreed to dismiss the case prior to trial")
+CaseResult.create(result: "DUI and Leaving the Scene of an Accident. Our client was acquitted following a jury trial.")
+CaseResult.create(result: "Harassment - Domestic Violence. Our client was granted a deferred sentence and the charges were dismissed. A conviction would have likely resulted in deportation proceedings.")
