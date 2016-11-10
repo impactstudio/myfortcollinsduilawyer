905f812f6f312854e3613cceb15660bae89135d1
theRingleman <sam.ringleman@gmail.com>
2016-11-10 09:31:48 -0700

    add titles to each page in the static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,25 +1,32 @@
 class StaticController < ApplicationController
   def home
+    set_meta_tags title: "Your Fort Collins DUI and Criminal Defense Lawyer"
   end
 
   def about
+    set_meta_tags title: "About"
   end
 
   def contact
+    set_meta_tags title: "Contact"
   end
 
   def thank_you
   end
 
   def fort_collins_dui_lawyer
+    set_meta_tags title: "Fort Collins DUI Lawyer"
   end
 
   def fort_collins_criminal_defense_lawyer
+    set_meta_tags title: "Fort Collins Criminal Defense Lawyer"
   end
 
   def case_results
+    set_meta_tags title: "Case Results"
   end
 
   def privacy_policy
+    set_meta_tags title: "Privacy Policy"
   end
 end
