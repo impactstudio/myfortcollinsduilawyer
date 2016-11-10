793ab3d5d0b4ca866ef761d525b4af96dbc32b0c
theRingleman <sam.ringleman@gmail.com>
2016-11-10 12:46:34 -0700

    set up the criminal defense controller

--- app/controllers/criminal_defenses_controller.rb
+++ app/controllers/criminal_defenses_controller.rb
@@ -1,6 +1,41 @@
 class CriminalDefensesController < ApplicationController
+  before_action :set_criminal_defense, only: [:show, :edit, :update]
+
   def show
-    @criminal_defense = CriminalDefense.friendly.find(params[:id])
-    set_meta_tags title: @criminal_defense.title
+    set_meta_tags title: "Fort Collins Criminal Defense Lawyer | #{@criminal_defense.title}",
+                  author: "Eric A. Sunness"
   end
+
+  def new
+    @criminal_defense = Dui.new
+  end
+
+  def create
+    @dcd = Dui.create(dui_params)
+    if @criminal_defense.save
+      redirect_to @criminal_defense
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @criminal_defense.update(dui_params)
+    @criminal_defense.save
+    redirect_to @criminal_defense
+  end
+
+  private
+
+  def set_dui
+    @criminal_defense = CriminalDefense.friendly.find(params[:id])
+  end
+
+  def dui_params
+    params.require(:criminal_defense).permit(:title, :content, :description, :tags)
+  end
+
 end
